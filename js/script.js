23f3173b5c7458965636c17ce87c21af322dae05
Yunki0 <desirelionel109@gmail.com>
2024-05-18 12:09:36 +0000

    third commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarLinks = document.querySelectorAll('#navbar a');
+
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
